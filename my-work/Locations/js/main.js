8662d440073826a16285fdc31e9844fd9819f1c8
Artem Zhylko <you@example.com>
2016-09-02 01:23:46 +0300

    Update commit

--- my-work/Locations/js/main.js
+++ my-work/Locations/js/main.js
@@ -2,29 +2,29 @@
 
 var App = App || {};
 
+loadJSON(function(response) { // Load locations.json.
+    var jsonResponse = JSON.parse(response);
+
+    App.data = jsonResponse;
+});
+
 function start () { // Application Entry Point.
-    loadJSON(function(response) { // Load locations.json.
-        var jsonResponse = JSON.parse(response);
+    var controller = new App.Controller();
 
-        App.data = jsonResponse;
-    });
-    
-            var controller = new App.Controller();
-        
-        controller.init(); // Run the controller
+    controller.init(); // Run the controller
+}
 
-    function loadJSON(callback) {
-        var xobj = new XMLHttpRequest();
-    
-        xobj.overrideMimeType('application/json');
-        xobj.open('GET', 'js/db/locations.json', true);
-        xobj.onreadystatechange = function () {
-            if (xobj.readyState == 4 && xobj.status == '200') {
-                callback(xobj.responseText);
-            }
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+
+    xobj.overrideMimeType('application/json');
+    xobj.open('GET', 'js/db/locations.json', true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == '200') {
+            callback(xobj.responseText);
         }
-        xobj.send(null);
     }
+    xobj.send(null);
 }
 
 document.addEventListener('DOMContentLoaded', start, false);
