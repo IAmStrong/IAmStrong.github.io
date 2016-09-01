a3b79f134469030401894ee5cd1baa40d6d25808
Artem Zhylko <you@example.com>
2016-09-02 00:44:43 +0300

    Update commit

--- my-work/Locations/js/main.js
+++ my-work/Locations/js/main.js
@@ -5,8 +5,26 @@ var App = App || {};
 function start () { // Application Entry Point.
     var controller = new App.Controller();
 
-    App.loadData();
-    controller.init();
+    loadJSON(function(response) { // Load locations.json.
+        var jsonResponse = JSON.parse(response);
+
+        App.data = jsonResponse;
+    });
+    
+    controller.init(); // Run the controller
+
+    function loadJSON(callback) {
+        var xobj = new XMLHttpRequest();
+    
+        xobj.overrideMimeType('application/json');
+        xobj.open('GET', 'js/db/locations.json', true);
+        xobj.onreadystatechange = function () {
+            if (xobj.readyState == 4 && xobj.status == '200') {
+                callback(xobj.responseText);
+            }
+        }
+        xobj.send(null);
+    }
 }
 
 document.addEventListener('DOMContentLoaded', start, false);
